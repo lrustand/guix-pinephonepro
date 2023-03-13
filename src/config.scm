232c916c12289fc03fef5b49df12ccd55a29c4a2
Andrew Tropin <andrew@trop.in>
2023-03-13 13:13:47 +0400

    Add pinephone pro firmwares

--- src/config.scm
+++ src/config.scm
@@ -1,4 +1,49 @@
 (define-module (config)
-  #:use-module (gnu packages certs))
+  #:use-module (gnu packages certs)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system copy)
+  #:use-module (nonguix licenses))
 
-(define-public pinephone-kernel nss-certs)
+(define pinephone-pro-firmware
+  (let ((commit "5c4c2b89f30a42f5ffabb5b5bcbc799d8ac9f66f")
+        (revision "1"))
+    (package
+      (name "pinephone-pro-firmware")
+      (version (git-version "0.0.0" revision commit))
+      (home-page "https://megous.com/git/linux-firmware")
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url home-page)
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "0210dpxhb257zwncv5r1qiq7rlyiy1c14mx9vscnsv6rggf1id9w"))))
+      (build-system copy-build-system)
+      (arguments
+       `(#:install-plan
+         (list
+          (list "anx7688-fw.bin" "lib/firmware/")
+          (list "hm5065-af.bin" "lib/firmware/")
+          (list "hm5065-init.bin" "lib/firmware/")
+          (list "ov5640_af.bin" "lib/firmware/")
+          (list "regulatory.db" "lib/firmware/")
+          (list "regulatory.db.p7s" "lib/firmware/")
+          (list "rockchip" "lib/firmware/")
+          (list "rt2870.bin" "lib/firmware/")
+          (list "rtl_bt" "lib/firmware/")
+          (list "rtlwifi" "lib/firmware/")
+          (list "rtw88" "lib/firmware/")
+          (list "rtw89" "lib/firmware/")
+          (list "brcm" "lib/firmware/"))))
+      (synopsis "Nonfree Linux firmware blobs for PinePhone Pro")
+      (description "Nonfree Linux firmware blobs for PinePhone Pro.")
+      (license
+       (nonfree
+        (string-append "https://git.kernel.org/pub/scm/linux/kernel/git/"
+                       "firmware/linux-firmware.git/plain/WHENCE"))))))
+
+
+(define-public pinephone-kernel pinephone-pro-firmware)
